b0038d14ecea690ff3a3762399d2169c82ea8cb0
kbrady-rgb <58741813+kbrady-rgb@users.noreply.github.com>
2021-02-12 13:37:39 -0600

    Initialization

---
+++ Queue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Queue/queueFullException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QueueLab
+{
+    public class queueFullException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Queue is Full";
+            }
+        }
+    }
+}
